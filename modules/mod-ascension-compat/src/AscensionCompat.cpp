223cedc679d3e36b7a5283a1c8d1ae8228496ae0
Mrpirolito <mr.pirolito@gmail.com>
2026-09-22 15:54:29 +0200

    fix(CoA): drop the byte order mark from AscensionCompat.cpp
    A PowerShell rewrite left a BOM on the file. comment_policy only exempts a legal notice that starts the file, and a BOM in front of it means the notice no longer does, so source-boundaries rejected the header.

--- modules/mod-ascension-compat/src/AscensionCompat.cpp
+++ modules/mod-ascension-compat/src/AscensionCompat.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU
  * AGPL v3 license:
  * https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
